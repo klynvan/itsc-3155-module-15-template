9e59b6d9007d8b1afc689ba7f3a4c8be668564b4
Kaitlyn <kaitlynavan@gmail.com>
2022-12-14 18:22:34 -0500

    test workflow

--- tests/test_movie_routes.py
+++ tests/test_movie_routes.py
@@ -16,7 +16,7 @@ def test_get_all_movies(test_app: FlaskClient):
 
     # asserts
     assert res.status_code == 200
-    assert '<td><a href="/movies/{{ movie.movie_id }}">Minions</a></td>' in page_data
+    assert f'<td><a href="/movies/{test_movie.movie_id}">Minions</a></td>' in page_data
     assert ' <td>Kyle Balda</td>' in page_data
     assert ' <td>4</td>' in page_data
 
@@ -32,3 +32,26 @@ def test_get_all_movies_empty(test_app: FlaskClient):
 
     assert res.status_code == 200
     assert '<td>' not in page_data
+
+
+def test_get_single_movie_404(test_app: FlaskClient):
+    refresh_db()
+
+    res = test_app.get('/movie/1')
+
+    assert res.status_code == 404
+
+
+# def test_create_movie(test_app: FlaskClient):
+    # refresh_db()
+
+    # res = test_app.post('/movies', data={
+    # 'title': 'The Dark Knight',
+    # 'director': 'Christopher',
+    # 'rating': 5,
+    # }, follow_redirect=True)
+    #page_data = res.data.decode()
+
+    #assert res.status_code == 200
+    #assert '<h1> The Dark Knight - 4 </h1>' in page_data
+    #assert '<h2> Christopher Nolan</h2>' in page_data
