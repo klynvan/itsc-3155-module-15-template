c047b439cf8a2abf93f67ed14976a272998daab0
Kaitlyn <kaitlynavan@gmail.com>
2022-12-14 19:17:06 -0500

    updated

--- tests/utils.py
+++ tests/utils.py
@@ -3,3 +3,9 @@ from src.models import Movie, db
 def refresh_db():
     Movie.query.delete()
     db.session.commit()
+
+def create_movie(title='The Dark Knight', director = 'Christopher Nolan', rating = 5 ) -> Movie:
+    test_movie = Movie(titl =title, director=director, rating=rating)
+    db.session.add(test_movie)
+    db.session.commit()
+    return test_movie
